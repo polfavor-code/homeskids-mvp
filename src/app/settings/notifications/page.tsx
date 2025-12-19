81f0535e2a2a0fed1d351e4607c47da1d71b36cc
Paul <info@paulsnetwork.com>
2025-12-19 14:24:01 +0100

    Fix Uint8Array type cast for applicationServerKey

--- src/app/settings/notifications/page.tsx
+++ src/app/settings/notifications/page.tsx
@@ -142,7 +142,7 @@ export default function NotificationsSettingsPage() {
             // Subscribe to push
             const subscription = await registration.pushManager.subscribe({
                 userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+                applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as BufferSource,
             });
 
             const subJson = subscription.toJSON();
